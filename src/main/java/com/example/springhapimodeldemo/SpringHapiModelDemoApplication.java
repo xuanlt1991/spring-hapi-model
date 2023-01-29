5468d9eb45e7cd42a99a8fa69d30863f5ea73da8
xuanlt1991 <xuanlt1991@gmail.com>
2023-01-29 13:30:50 +0700

    first commit

---
+++ src/main/java/com/example/springhapimodeldemo/SpringHapiModelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhapimodeldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHapiModelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHapiModelDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springhapimodeldemo/SpringHapiModelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhapimodeldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHapiModelDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
